711ff30dff6b651e8b0fa0242b3e54972cf06708
julianalmandos <julianalmandos@gmail.com>
2019-12-27 01:35:36 -0300

    Added FontAwesome

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPencilAlt, faTrashAlt, faLink } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faPencilAlt,faTrashAlt,faLink)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
