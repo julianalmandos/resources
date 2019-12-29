bdd73f6ed401f737e1a4492dd991b024b032a287
julianalmandos <julianalmandos@gmail.com>
2019-12-29 04:09:11 -0300

    Started to migrate everything to Vuex stores

--- src/store.js
+++ src/store.js
@@ -1,16 +1,50 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    resources: [],
+    categories: [],
+    searchValue: '',
+    searchCategories: []
+  },
+  getters: {
+    getFilteredResources(state) {
+      //Filter code
+      return state.resources.filter(resource => {
+        return resource.title.toLowerCase().includes(state.searchValue.toLowerCase());
+      });
+    },
+    getFilteredResourcesCount(state, getters) {
+      return getters.getFilteredResources.length;
+    }
   },
   mutations: {
-
+    getResources(state, resources) {
+      state.resources = resources;
+    },
+    getCategories(state, categories) {
+      state.categories = categories;
+    },
+    setSearchValue(state, value) {
+      state.searchValue = value;
+    }
   },
   actions: {
-
+    getResources({ commit }) {
+      Axios.get('//localhost:3000/api/resources')
+        .then(response => {
+          commit('getResources', response.data);
+        })
+    },
+    getCategories({ commit }) {
+      Axios.get('//localhost:3000/api/categories')
+        .then(response => {
+          commit('getCategories', response.data);
+        })
+    },
   }
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,6 @@
 <template>
   <div class="home">
-    <h3 class="search-info" v-if="searchValue!=''">
-      Searching for "{{searchValue}}" / {{currentResources.length}} results
-      <a @click="clearSearch()" class="clear">clear</a>
-    </h3>
-    <input id="search" type="text" placeholder="Buscar..." v-model="searchValue" />
-    <h3 class="search-category-info" v-if="selectedCategories.length!=0">
-      Searching for
-      <span v-for="(category,index) of selectedCategories" :key="index">
-        <span v-if="index!=0 && index!=selectedCategories.length-1">,</span>
-        <span v-if="index==selectedCategories.length-1 && selectedCategories.length>1">and</span>
-        {{getCategoryName(category)}}
-      </span>
-
-      / {{currentResources.length}} results
-      <a @click="clearCategories()" class="clear">clear</a>
-    </h3>
-    <div class="categories">
-      <Category
-        v-for="(category,index) of categories"
-        :key="index"
-        :category="category"
-        @select="selectCategory(category.id)"
-      />
-    </div>
+	<Filtered/>
     <div
       v-if="currentResources.length!=0"
       class="grid"
@@ -38,94 +15,33 @@
 </template>
 
 <script>
-// @ is an alias to /src
 import Resource from "@/components/Resource.vue";
-import Category from "@/components/Category.vue";
-import axios from "axios";
+import Filtered from "@/components/Filter.vue";
+import Axios from "axios";
+import Vuex from "vuex";
 
 export default {
   name: "home",
   components: {
-    Resource,
-    Category
+	Resource,
+	Filtered,
   },
   data() {
-    return {
-      resources: [],
-      currentResources: [],
-      categories: [],
-      selectedCategories: [],
-      searchValue: "",
-      totalResources: 0
-    };
-  },
-  watch: {
-    searchValue: function(val, oldVal) {
-      this.filterResources();
-    },
-    selectedCategories: function(val, oldVal) {
-      this.filterResources();
-    }
+	  return {
+
+	  }
   },
   beforeMount() {
-    //Llamada axios y me traigo todos los recursos
-    axios.get("http://localhost:3000/api/resources").then(response => {
-      console.log(response.data);
-      this.resources = response.data;
-      this.currentResources = this.resources;
-      this.totalResources = this.resources.length;
-    });
-    axios.get("http://localhost:3000/api/categories").then(response => {
-      this.categories = response.data;
-    });
+	//Llamada axios y me traigo todos los recursos
+	this.$store.dispatch('getResources');
   },
-  methods: {
-    selectCategory(id) {
-      if (this.selectedCategories.includes(id)) {
-        this.selectedCategories = this.selectedCategories.filter(
-          cat => cat != id
-        );
-      } else {
-        this.selectedCategories.push(id);
-      }
-    },
-    filterResources() {
-      if (this.searchValue != "" && this.selectedCategories.length != 0) {
-        //Search by both
-        this.currentResources = this.resources.filter(res => {
-          return (
-            res.title.includes(this.searchValue) &&
-            this.selectedCategories.includes(res.category.id)
-          );
-        });
-      } else if (this.searchValue != "") {
-        //Search by input
-        this.currentResources = this.resources.filter(res =>
-          res.title.includes(this.searchValue)
-        );
-      } else if (this.selectedCategories.length != 0) {
-        //Search by category
-        this.currentResources = this.resources.filter(res =>
-          this.selectedCategories.includes(res.category.id)
-        );
-      } else {
-        //Reset
-        this.currentResources = this.resources;
-      }
-    },
-    getCategoryName(categoryId) {
-      let category = this.categories.find(
-        currentCategory => currentCategory.id == categoryId
-      );
-      return category.name;
-    },
-    clearCategories() {
-      // WIP
-    },
-    clearSearch() {
-      this.searchValue = "";
-    }
-  }
+  computed: {
+	  ...Vuex.mapState({
+		  currentResources() {
+			  return this.$store.getters.getFilteredResources;
+		  }
+	  }),
+  },
 };
 </script>
 
@@ -134,46 +50,6 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
-}
-
-.categories {
-  display: flex;
-  width: 100%;
-  flex-direction: row;
-  justify-content: space-evenly;
-  margin-top: 30px;
-}
-
-.clear {
-  color: #cccccc;
-  text-decoration: none;
-}
-
-.clear:hover {
-  color: #2c3e50;
-  cursor: pointer;
-}
-
-#search {
-  width: 60%;
-  height: 2.5rem;
-  border: 1px solid #bbbbbb;
-  border-radius: 5px;
-  box-sizing: border-box;
-  padding: 12px;
-  font-size: 16px;
-}
-
-.error {
-  margin-top: 30px;
-}
-
-.search-info {
-  margin-top: 0px;
-}
-
-.search-category-info {
-  margin-bottom: 0px;
 }
 
 .grid {
@@ -202,15 +78,6 @@ export default {
 @media (max-width: 800px) {
   .grid {
     grid-template-columns: 1fr;
-  }
-
-  .categories {
-    flex-direction: column;
-    margin-top: 10px;
-  }
-
-  #search {
-    width: 100%;
   }
 }
 </style>
