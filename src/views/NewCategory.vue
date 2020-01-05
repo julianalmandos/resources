7d11caaf7ecbcd09b8ec5cf7b9b698ea14859fb5
julianalmandos <julianalmandos@gmail.com>
2020-01-05 00:59:58 -0300

    Created main structure of category creation component

--- src/views/NewCategory.vue
+++ src/views/NewCategory.vue
@@ -1,5 +1,20 @@
 <template>
     <div class="new-category">
+        <h2><font-awesome-icon icon="plus"/> New Category</h2>
+        <form class="form-a" action="">
+            <div class="box">
+                <div class="box__additional">
+                    Name
+                </div>
+                <input class="box__input" type="text" placeholder="" v-model="category.name" />
+            </div>
+            <div class="box">
+                <div class="box__additional">
+                    Color
+                </div>
+                <input class="box__input" type="text" placeholder="" v-model="category.color" />
+            </div>
+        </form>
     </div>
 </template>
 
@@ -18,5 +33,55 @@ export default {
 </script>
 
 <style scoped>
+    .new-category {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
 
+    .form-a {
+        padding: 50px;
+        border-left: 1px dashed #2c3e50;
+        border-right: 1px dashed #2c3e50;
+        border-radius: 15px;
+    }
+
+    .box {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 300px;
+    }
+
+    .box:first-child {
+        margin-bottom: 30px;
+    }
+
+    .box__input {
+        width: 750px;
+        height: 2.5em;
+        border: 2px solid #2c3e50;
+        box-sizing: border-box;
+        padding: 12px;
+        font-size: 15px;
+    }
+
+    .box__input:focus {
+        border: 2px solid #60758b;
+        outline: none;
+    }
+
+    .box__input-info {
+        margin: 0px;
+    }
+
+    .box__additional {
+        padding: 12px;
+        display: flex;
+        align-items: center;
+        height: 2.5em;
+        box-sizing: border-box;
+        background-color: #2c3e50;
+        color: white;
+    }
 </style>
