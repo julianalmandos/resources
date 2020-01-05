32906c4c32312fad37b89752c1543b04cfcde229
julianalmandos <julianalmandos@gmail.com>
2020-01-04 23:45:15 -0300

    Created initial structure for entity creation views

---
+++ src/views/NewCategory.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="new-category">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NewCategory',
+    data() {
+        return {
+            category: {
+                name: '',
+                color: '',
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/NewResource.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="new-resource">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NewResource',
+    data() {
+        return {
+            resource: {
+                url: '',
+                title: '',
+                description: '',
+                categoryId: '',
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
