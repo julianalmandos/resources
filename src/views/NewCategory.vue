44b79a9ce98c8f52e74ce4125e52c2f6d4490f94
julianalmandos <julianalmandos@gmail.com>
2020-01-06 23:51:55 -0300

    Finished new category's form initial style

--- src/views/NewCategory.vue
+++ src/views/NewCategory.vue
@@ -1,19 +1,22 @@
 <template>
     <div class="new-category">
         <h2><font-awesome-icon icon="plus"/> New Category</h2>
-        <form class="form-a" action="">
-            <div class="box">
-                <div class="box__additional">
-                    Name
+        <form @submit.stop.prevent="createCategory()" class="form" action="">
+            <div class="form__inputs">
+                <div class="box">
+                    <div class="box__additional">
+                        Name
+                    </div>
+                    <input class="box__input" type="text" placeholder="" v-model="category.name" />
                 </div>
-                <input class="box__input" type="text" placeholder="" v-model="category.name" />
-            </div>
-            <div class="box">
-                <div class="box__additional">
-                    Color
+                <div class="box">
+                    <div class="box__additional">
+                        Color
+                    </div>
+                    <input class="box__input" type="text" placeholder="" v-model="category.color" />
                 </div>
-                <input class="box__input" type="text" placeholder="" v-model="category.color" />
             </div>
+            <button class="form__button" type="submit"><font-awesome-icon icon="plus"/> Create!</button>
         </form>
     </div>
 </template>
@@ -39,11 +42,34 @@ export default {
         align-items: center;
     }
 
-    .form-a {
+    .form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .form__inputs {
         padding: 50px;
         border-left: 1px dashed #2c3e50;
         border-right: 1px dashed #2c3e50;
         border-radius: 15px;
+    }
+
+    .form__button {
+        padding: 12px 18px 12px 18px;
+        background-color: #eeeeee;
+        color: #2c3e50;
+        border: 1px solid #2c3e50;
+    }
+
+    .form__button:hover {
+        background-color: #2c3e50;
+        color: #eeeeee;
+        cursor: pointer;
+    }
+
+    .form__button:focus {
+        outline: none;
     }
 
     .box {
